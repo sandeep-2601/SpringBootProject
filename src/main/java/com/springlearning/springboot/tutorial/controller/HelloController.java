79fdd1a01653e8f5cf96650000c7776e9863b02d
=sandy-45 <=>
2022-01-20 21:58:10 +0530

    Basic Rest Controller

---
+++ src/main/java/com/springlearning/springboot/tutorial/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.springlearning.springboot.tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//    @RequestMapping(value="/",method= RequestMethod.GET)
+    @GetMapping("/hello") //You can add value as a kw
+    public String helloWorld() {
+        return "Welcome  to my first rest api";
+    }
+}
